09161e2dd548d9f00b38fc6df31d118899b68176
charisadihara <muhammad.charis@redcomm.co.id>
2024-07-26 05:15:58 +0700

    feat: init page

---
+++ pages/spin/point/[randomCode].vue
@@ -0,0 +1,7 @@
+<template>
+  <div
+    class="grow bg-[url('assets/images/bg-green.webp')] bg-cover bg-center relative"
+  >
+    <NuxtImg src="/logo.webp" alt="logo" width="190" height="71" preload />
+  </div>
+</template>
