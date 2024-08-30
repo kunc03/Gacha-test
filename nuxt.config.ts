2b06d72118b4cb2bd968a40958fe757798f31d82
Septiyana Dewi <septiyana.dewi@talenavi.com>
2024-08-30 15:47:47 +0700

    fix: [SD] add function to share each button and setup the favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,6 +12,10 @@ export default defineNuxtConfig({
           rel: 'stylesheet',
           href: 'https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap',
         },
+        { rel: 'icon', type: 'image/x-icon', href: '/logo-icon.ico' },
+        // { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        // { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        // { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
       ],
     },
   },
@@ -59,6 +63,11 @@ export default defineNuxtConfig({
       BASE_API: process.env.NUXT_BASE_API,
       API_URL: process.env.API_URL,
       GOOGLE_API: process.env.GOOGLE_API,
+      APP_NAME: process.env.APP_NAME,
+      META_TITLE: process.env.META_TITLE,
+      META_DESCRIPTION: process.env.META_DESCRIPTION,
+      META_IMAGE: process.env.META_IMAGE,
+      META_URL: process.env.META_URL
     },
   },
 
