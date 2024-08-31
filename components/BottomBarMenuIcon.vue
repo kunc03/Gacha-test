cbe2f9b1710a60e7a45e63d2a841a18c2abf262c
charistalenavi <muhammad.charis@talenavi.com>
2024-08-31 10:43:56 +0700

    feat: add reusable bottom bar

---
+++ components/BottomBarMenuIcon.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="flex flex-col items-center justify-center">
+    <img
+      :src="icon"
+      alt="icon"
+      width="30"
+      height="30"
+      preload
+      class="text-center cursor-pointer"
+      @click="handleClick"
+    />
+    <p class="text-exd-gray-scorpion text-exd-1020">{{ label }}</p>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  icon: {
+    type: String,
+    required: true,
+  },
+  label: {
+    type: String,
+    required: true,
+  },
+  onClick: {
+    type: Function,
+    default: null,
+  },
+})
+
+const handleClick = (event) => {
+  if (props.onClick) {
+    props.onClick(event)
+  }
+}
+</script>
