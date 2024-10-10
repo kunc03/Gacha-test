893d80e01e108284d8207f81b8cf3580186a26d8
Bagus Kuncoro <bagus.kuncoro@talenavi.com>
2024-10-11 06:06:00 +0700

    fix: active tag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -43,7 +43,7 @@ export default defineNuxtConfig({
   ],
 
   gtag: {
-    // enabled: process.env.NODE_ENV === 'production',
+    enabled: process.env.NODE_ENV === 'production',
     id: process.env.NUXT_PUBLIC_GTAG_ID,
   },
 
